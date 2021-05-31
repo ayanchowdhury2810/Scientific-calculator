78c1a000ee005a829ab076f7a0fe1397d767bd06
Ayan Chowdhury <72807747+ayanchowdhury2810@users.noreply.github.com>
2021-05-31 11:38:06 +0530

    Update main.py

--- Scientific Calculator/main.py
+++ Scientific Calculator/main.py
@@ -283,18 +283,6 @@ filemenu.add_separator()
 
 filemenu.add_command(label="Exit", command = iExit)
 
-# Edit tab
-editmenu= Menu(menubar, tearoff=0)
-menubar.add_cascade(label= "Edit", menu=editmenu)
-editmenu.add_command(label="Cut ")
-editmenu.add_command(label="Copy ")
-editmenu.add_command(label="Paste ")
-
-# Help tab
-helpmenu= Menu(menubar, tearoff=0)
-menubar.add_cascade(label= "Help", menu=helpmenu)
-helpmenu.add_command(label="View Help")
-
 
 root.config(menu=menubar)
 root.mainloop()
